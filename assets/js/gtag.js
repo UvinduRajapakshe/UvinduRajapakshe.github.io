49ec36cee3f1a538b112d18c6c3fc5088671e3ac
Uvindu Bro <79355885+UvinduBro@users.noreply.github.com>
2021-07-29 09:33:41 +0530

    Update gtag.js

--- assets/js/gtag.js
+++ assets/js/gtag.js
@@ -1,1 +1,10 @@
-G-4QZSXQ1E5SG-4QZSXQ1E5S
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4QZSXQ1E5S"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){window.dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4QZSXQ1E5S');
+</script>
+
